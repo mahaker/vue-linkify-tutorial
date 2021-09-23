50dce95e08bf04a2c7957191d453668ab6ccaf40
Hideaki Matsunami <hideaki.matsunami@esm.co.jp>
2021-09-23 11:03:18 +0900

    add v2 project

---
+++ v2project/src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    {{ htmlString }}
+    <v-linkify :html="htmlString" />
+    <div v-html="scriptString"></div>
+    <v-linkify :html="scriptString" />
+  </div>
+</template>
+
+<script>
+import vLinkify from 'vue-linkify-v2';
+
+export default {
+  name: 'HelloWorld',
+  components: {
+    vLinkify
+  },
+  data: function() {
+    return {
+      htmlString: 'For help with http://github.com.',
+      scriptString: '<div onmouseover="alert(1+1)">focus</div>',
+    }
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ v3project/src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hello">
+    {{ msg }}
+    <v-linkify :html="htmlString" />
+    <div v-html="scriptString"></div>
+    <v-linkify :html="scriptString" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+// @ts-ignore
+import vLinkify from 'vue-linkify-v2';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  components: {
+    vLinkify
+  },
+  data: function() {
+    return {
+      htmlString: 'For help with http://github.com.',
+      scriptString: '<div onmouseover="alert(1+1)">focus</div>',
+    }
+  },
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
